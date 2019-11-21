66ce54ca29880da2a3984b8ddb65829d029e1453
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-11-21 07:52:17 +0100

    setup: Fix python classifier

--- setup.py
+++ setup.py
@@ -34,8 +34,10 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description="Mythen SLS detector interface",
     entry_points={
