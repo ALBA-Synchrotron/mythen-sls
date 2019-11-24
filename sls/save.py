50f18e56802a62a91663bcc67deaff9d0373fa47
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2019-11-24 12:52:35 +0100

    save: Add support for .npy binary files

--- sls/save.py
+++ sls/save.py
@@ -6,5 +6,7 @@ def save(frame, filename):
         data = '\n'.join('%d %d' % (n, p) for n, p in enumerate(frame))
         with open(filename, 'wt') as f:
             f.write(data)
+    elif filename.endswith('.npy'):
+        numpy.save(filename, frame)
     else:
         raise ValueError('Unsupported format')
