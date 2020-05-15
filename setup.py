43e10ed16aeb87097fdab0b765e0243635854553
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2020-05-15 12:45:24 +0200

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     extras_require=extras_requirements,
     python_requires=">=3.5",
     url="https://github.com/alba-synchrotron/sls-detector",
-    version="0.1.0",
+    version="1.0.0",
     zip_safe=True
 )
